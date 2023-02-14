a413ce0656e2461cffbb663aae1e93b63d61807c
houssem <lahmarhoussem90@gmail.com>
2023-02-14 18:58:00 +0100

    refactor calling endpoints

--- src/main/scala/solution/RestAPiClient.scala
+++ src/main/scala/solution/RestAPiClient.scala
@@ -3,7 +3,7 @@ package solution
 
 
 import scala.concurrent.duration._
-import scala.concurrent.{Await, Future}
+import scala.concurrent.{Await, ExecutionContext, Future}
 import scala.util.{Failure, Success, Try}
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
@@ -38,6 +38,9 @@ case class BikeInfo(date_stolen:Long, description:String, frame_colors:List[Stri
 case class BikeArray(bikes: java.util.List[BikeInfo])
 
 object RestApiClient {
+
+
+
   def main(args: Array[String]): Unit = {
     implicit val system = ActorSystem()
     implicit val materializer = ActorMaterializer()
@@ -49,56 +52,14 @@ object RestApiClient {
     val url = "https://bikeindex.org/api/v3/search?page=1&per_page=1&location=address&stolenness=all"
 
     val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = url))
-    val gson = new Gson()
+
 
     system.scheduler.schedule(0.minutes, 5.minutes) {
-      responseFuture
-        .onComplete {
-          case Success(res) =>
-            res.entity.dataBytes.runFold(ByteString(""))(_ ++ _).map { body =>
-              val bikes = body.utf8String
-
-
-              val mapType = new TypeToken[java.util.HashMap[String, java.util.ArrayList[BikeInfo]]] {}.getType
-              val person = Try(gson.fromJson(body.utf8String,mapType).asInstanceOf[java.util.Map[String, java.util.ArrayList[BikeInfo]]])
-               person match {
-                case Success(output) => {
-                  val bike_id = output.get("bikes").get(0).id.toString
-                  //println(body.utf8String)
-                  sendToKafka(bikes, "bikes")
-
-
-                  //second end point
-
-                  val url_bikeInfo = "https://bikeindex.org/api/v3/bikes/" + bike_id
-
-                  val responseFuture2: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = url_bikeInfo))
-                  responseFuture2
-                    .onComplete {
-                      case Success(res) =>
-                        res.entity.dataBytes.runFold(ByteString(""))(_ ++ _).map { body =>
-                          val bike_id_info = body.utf8String
-                         // println(bike_id_info)
-                          sendToKafka(bike_id_info, "bike_info")
-
-                        }
-                      case Failure(_) => sys.error("something went wrong when searching the bike " + bike_id)
-                    }
-                }
-                case Failure(e) => {
-                  println(s"Error occurred: ${e.getMessage}")
-                }
-              }
-              println(body.utf8String)
-            }
-
-
-          case Failure(_) => sys.error("something went wrong")
-        }
-
+      callSearchBikeEndpoint(responseFuture)
     }
 
   }
+
   def sendToKafka(data: String,topic:String): Unit = {
     //val topic = "bikes"
     val brokers = "localhost:9092"
@@ -115,6 +76,63 @@ object RestApiClient {
   }
 
 
+
+//call first endpoint about searchingBike
+  def callSearchBikeEndpoint(responseFuture :Future[HttpResponse])(implicit executionContext: ExecutionContext,
+  system : ActorSystem, materializer: ActorMaterializer) = {
+    //implicit val system = ActorSystem()
+  //  implicit val materializer = ActorMaterializer()
+   // implicit val executionContext = system.dispatcher
+    val gson = new Gson()
+    responseFuture
+      .onComplete {
+        case Success(res) =>
+          res.entity.dataBytes.runFold(ByteString(""))(_ ++ _).map { body =>
+            val bikes = body.utf8String
+
+
+            val mapType = new TypeToken[java.util.HashMap[String, java.util.ArrayList[BikeInfo]]] {}.getType
+            val person = Try(gson.fromJson(body.utf8String, mapType).asInstanceOf[java.util.Map[String, java.util.ArrayList[BikeInfo]]])
+            person match {
+              case Success(output) => {
+                val bike_id = output.get("bikes").get(0).id.toString
+                //println(body.utf8String)
+                sendToKafka(bikes, "searchbikes")
+
+
+                callEnpointGetBikeById(bike_id)
+              }
+              case Failure(e) => {
+                println(s"Error occurred: ${e.getMessage}")
+              }
+            }
+            println(body.utf8String)
+          }
+
+
+        case Failure(_) => sys.error("something went wrong")
+      }
+  }
+  def callEnpointGetBikeById(bike_id: String)(implicit executionContext: ExecutionContext,
+                                              system: ActorSystem, materializer: ActorMaterializer) = {
+    //second end point
+
+
+    val url_bikeInfo = "https://bikeindex.org/api/v3/bikes/" + bike_id
+
+    val responseFuture2: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = url_bikeInfo))
+    responseFuture2
+      .onComplete {
+        case Success(res) =>
+          res.entity.dataBytes.runFold(ByteString(""))(_ ++ _).map { body =>
+            val bike_id_info = body.utf8String
+            println(bike_id_info)
+            sendToKafka(bike_id_info, "infobikes")
+
+          }
+        case Failure(_) => sys.error("something went wrong when searching the bike " + bike_id)
+      }
+  }
 }
 
 
