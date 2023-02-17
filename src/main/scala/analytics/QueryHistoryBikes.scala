4a6c823c1236a0c3c1a643c8e1cd8dd819160f82
houssem <lahmarhoussem90@gmail.com>
2023-02-17 07:07:20 +0100

    refine cleaning process

--- src/main/scala/analytics/QueryHistoryBikes.scala
+++ src/main/scala/analytics/QueryHistoryBikes.scala
@@ -14,16 +14,18 @@ object QueryHistoryBikes {
     //retrieve history
     val deltaTable = DeltaTable.forPath(spark, "file:/home/houssem/scala-workspace/BikesBigData/spark-warehouse/bikes")
 
+    //history dataframe
+    val fullHistoryDF = deltaTable.history() // get the full history of the table
+    fullHistoryDF.show(false)
 
-
-//check version 0
-//check content of first dataframe
-val timeTravelDF_0 = spark.read.format("delta")
-  .option("versionAsOf", 0)
-  .load("file:/home/houssem/scala-workspace/BikesBigData/spark-warehouse/bikes")
+    //check version 0
+    //check content of first dataframe
+    val timeTravelDF_0 = spark.read.format("delta")
+      .option("versionAsOf", 0)
+      .load("file:/home/houssem/scala-workspace/BikesBigData/spark-warehouse/bikes")
     timeTravelDF_0.show(5, false)
 
-  val bike_id_to_update= timeTravelDF_0.head().getAs[Long](4)
+    val bike_id_to_update = timeTravelDF_0.head().getAs[Long](4)
 
 
     //update data
@@ -31,20 +33,12 @@ val timeTravelDF_0 = spark.read.format("delta")
       col("id") === bike_id_to_update,
       Map("frame_model" -> lit("click2")))
 
- //check the history of transformations
-    val fullHistoryDF = deltaTable.history() // get the full history of the table
-
+    //check the history of transformations
     fullHistoryDF.show(fullHistoryDF.count().toInt, false)
 
 
     val lastOperationDF = deltaTable.history(1)
     lastOperationDF.show(fullHistoryDF.count().toInt, false)
-
-
-
-
-
-
 
 
     //check content of last dataframe
@@ -56,23 +50,19 @@ val timeTravelDF_0 = spark.read.format("delta")
 
 
 
-
-
-
-//difference between two versions
-    timeTravelDF_1 .except(timeTravelDF_0).show(false)
+    //difference between two versions
+    timeTravelDF_1.except(timeTravelDF_0).show(false)
 
     val df_diff = spark.read.format("delta")
       .option("readChangeFeed", "true")
       .option("startingVersion", 0)
-      .option("endingVersion",1)
+      .option("endingVersion", 1)
       //.table("myDeltaTable")
       .load("file:/home/houssem/scala-workspace/BikesBigData/spark-warehouse/bikes")
 
     df_diff.printSchema()
     df_diff.show(false)
     print("end")
-
 
 
   }
